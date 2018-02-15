0d54d90d3cfbc63775f224d62731a500886f9b86
Brett Nelson <brettcnelson@gmail.com>
2018-02-14 21:50:52 -0500

    auto play without symmetry

--- src/Board.js
+++ src/Board.js
@@ -3,6 +3,7 @@ import './Board.css';
 import Square from './Square';
 
 function Board(props) {
+	var symms = [[1,2,3,4,5,6,7,8,9],[7,4,1,8,5,2,9,6,3],[9,8,7,6,5,4,3,2,1],[3,6,9,2,5,8,1,4,7],[3,2,1,6,5,4,9,8,7],[9,6,3,8,5,2,7,4,1],[7,8,9,4,5,6,1,2,3],[1,4,7,2,5,8,3,6,9]];
 	var squares = [[1,2,3],[4,5,6],[7,8,9]];
 
 	function makeBoard() {
@@ -44,7 +45,6 @@ function Board(props) {
 	}
 
 	function makeWinBoard(s,i) {
-		var symms = [[1,2,3,4,5,6,7,8,9],[7,4,1,8,5,2,9,6,3],[9,8,7,6,5,4,3,2,1],[3,6,9,2,5,8,1,4,7],[3,2,1,6,5,4,9,8,7],[9,6,3,8,5,2,7,4,1],[7,8,9,4,5,6,1,2,3],[1,4,7,2,5,8,3,6,9]];
 		var val = props.node.board[s];
 		return props.node.res.some(w=>w===s) ?
 			<Square key={i} s={s} val={val} color={'red'} /> :
--- src/Train.js
+++ src/Train.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Game from './Game';
+import React, { Component } from 'react';
+import data from './data';
+import Display from './Display';
+import Board from './Board';
 
-function Train(props) {
-  return (
-    <div className="Train">
-    	<div><button onClick={props.click} >play against the computer</button></div>
-    	<Game />
-    </div>
-  );
+class Train extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      game:0,
+      move:0,
+      symm:0
+    };
+  }
+
+  render() {
+    var delay = 500;
+    var currGame = data.games[this.state.game];
+    if (currGame.moves[this.state.move+1]) {
+      var move = this.state.move;
+      setTimeout(()=>this.setState({move:move+1}),delay);
+    }
+    else if (data.games[this.state.game+1]) {
+      var game = this.state.game;
+      setTimeout(()=>this.setState({game:game+1,move:0}),delay)
+    }
+    var node = currGame.moves[this.state.move];
+
+    return (
+      <div className="Train">
+      	<div><button onClick={this.props.click} >play against the computer</button></div>
+        <div>Game {this.state.game+1} of {data.games.length} - LeafID: {currGame.leafID} - Res: {currGame.res} - Moves: {currGame.moves.length-1}</div>
+      	<Display node={node} />
+        <Board node={node} symm={this.state.symm} />
+      </div>
+    );
+  }
 }
 
 export default Train;
