3bb8945ded0a31c9647f40f1b77236b3d2613c03
Brett Nelson <brettcnelson@gmail.com>
2018-02-14 23:59:43 -0500

    adds speed change

--- src/Train.js
+++ src/Train.js
@@ -11,13 +11,19 @@ class Train extends Component {
       game:0,
       move:0,
       symm:0,
-      randSymm:false
+      randSymm:false,
+      delay: 1000
     };
   }
 
   togglePause() {
     var paused = this.state.paused;
     this.setState({paused:!paused});
+  }
+
+  changeSpeed() {
+    var speed = prompt('ms b/w moves');
+    this.setState({delay:speed,paused:true});
   }
 
   changeSymm() {
@@ -32,21 +38,20 @@ class Train extends Component {
   render() {
     var currGame = data.games[this.state.game];
     if (!this.state.paused) {
-      var delay = 1000;
       if (currGame.moves[this.state.move+1]) {
         var move = this.state.move;
-        setTimeout(()=>!this.state.paused && this.setState({move:move+1}),delay);
+        setTimeout(()=>!this.state.paused && this.setState({move:move+1}),this.state.delay);
       }
       else if (data.games[this.state.game+1]) {
         var game = this.state.game;
         var symm = this.state.randSymm ? this.randomSymm() : this.state.symm;
-        setTimeout(()=>!this.state.paused && this.setState({game:game+1,move:0,symm:symm}),delay)
+        setTimeout(()=>!this.state.paused && this.setState({game:game+1,move:0,symm:symm}),this.state.delay);
       }
     }
     var node = currGame.moves[this.state.move];
     return (
       <div className="Train">
-      	<div><button onClick={this.props.click} >play against the computer</button><button onClick={()=>this.togglePause()}>{this.state.paused ? 'resume' : 'pause'}</button><button onClick={()=>this.changeSymm()}>change symm</button></div>
+      	<div><button onClick={this.props.click} >play against the computer</button><button onClick={()=>this.togglePause()}>{this.state.paused ? 'resume' : 'pause'}</button><button onClick={()=>this.changeSymm()}>change symm</button><button onClick={()=>this.changeSpeed()}>change speed</button></div>
         <div>Game {this.state.game+1} of {data.games.length} - LeafID: {currGame.leafID} - Res: {currGame.res} - Moves: {currGame.moves.length-1} - Symm: {this.state.symm}</div>
       	<Display node={node} />
         <Board node={node} symm={this.state.symm} />
